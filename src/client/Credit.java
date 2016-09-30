55902a8c4ad5f96a3d04b4a932d754c4c3bbb840
d.yuji <d.yuji.fm@gmail.com>
2016-09-30 12:54:48 +0900

    Creditの仮実装をしました

--- src/client/Credit.java
+++ src/client/Credit.java
@@ -1,37 +1,45 @@
 package client;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
 
 public class Credit extends BasicGameState {
 	private int state;
-	
+	Image credit_image;
+	// private Music credit;
 	Credit (int state) {
 		this.state = state;
 	}
-	
+
 	@Override
 	public void init(GameContainer arg0, StateBasedGame arg1)
 			throws SlickException {
-		// TODO 自動生成されたメソッド・スタブ
-		
+			credit_image = new Image("res/client/img/Scene/credit/credit.png");
+
 	}
 
 	@Override
 	public void render(GameContainer arg0, StateBasedGame arg1, Graphics arg2)
 			throws SlickException {
-		// TODO 自動生成されたメソッド・スタブ
-		
+				credit_image.draw();
+
 	}
 
 	@Override
 	public void update(GameContainer arg0, StateBasedGame arg1, int arg2)
 			throws SlickException {
-		// TODO 自動生成されたメソッド・スタブ
-		
+			if (arg0.getInput().isKeyPressed(Input.KEY_ESCAPE)) {
+					arg1.enterState(State.MENU, new FadeOutTransition(Color.black, 1000),
+					new FadeInTransition(Color.black, 1000));
+			}
 	}
 
 	@Override
