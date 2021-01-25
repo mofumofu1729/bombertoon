94f11bc134bf9c8f2ee7b8e97d81d784cb845d1a
mofumofuchan <you_oki1@yahoo.co.jp>
2021-01-25 21:50:51 +0900

    Google Java Style Guideに従ってスタイルを修正

--- src/client/network/MesgRecvThread.java
+++ src/client/network/MesgRecvThread.java
@@ -5,185 +5,188 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-
 import client.constants.Direction;
 import client.constants.Status;
 import client.data.FieldClient;
 import client.data.PlayerClient;
 import server.constants.Color;
 
-//メッセージ受信のためのスレッド
+// メッセージ受信のためのスレッド
 public class MesgRecvThread extends Thread {
-	TransmissionClient tc;
-	Socket socket;
-	PrintWriter out;// 出力用のライター
+    TransmissionClient tc;
+    Socket socket;
+    PrintWriter out; // 出力用のライター
 
-	public MesgRecvThread(TransmissionClient tc, Socket s) {
-		this.tc = tc;
-		socket = s;
-	}
+    public MesgRecvThread(TransmissionClient tc, Socket s) {
+        this.tc = tc;
+        socket = s;
+    }
 
-	// 通信状況を監視し，受信データによって動作する
-	public void run() {
-		try {
-			InputStreamReader sisr = new InputStreamReader(socket.getInputStream());
-			BufferedReader br = new BufferedReader(sisr);
-			out = new PrintWriter(socket.getOutputStream(), true);
+    // 通信状況を監視し，受信データによって動作する
+    public void run() {
+        try {
+            InputStreamReader sisr = new InputStreamReader(socket.getInputStream());
+            BufferedReader br = new BufferedReader(sisr);
+            out = new PrintWriter(socket.getOutputStream(), true);
 
-			messageLoop: while (true) {
-				String inputLine = br.readLine();
-				if (inputLine != null) {
-					int x, y;
-					String[] command = inputLine.split(":");
-					switch (command[0]) {
-					case "FIELD":
-						x = Integer.parseInt(command[1]);
-						y = Integer.parseInt(command[2]);
-						FieldClient field;
-						if (command[3].equals("BOMB")) {
-							field = new FieldClient(x, y, true, Status.NOTHING, strToColor(command[4]));
-						} else {
-							field = new FieldClient(x, y, false, strToStatus(command[3]), strToColor(command[4]));
-						}
+            messageLoop: while (true) {
+                String inputLine = br.readLine();
+                if (inputLine != null) {
+                    int x, y;
+                    String[] command = inputLine.split(":");
+                    switch (command[0]) {
+                        case "FIELD":
+                            x = Integer.parseInt(command[1]);
+                            y = Integer.parseInt(command[2]);
+                            FieldClient field;
+                            if (command[3].equals("BOMB")) {
+                                field = new FieldClient(x, y, true, Status.NOTHING,
+                                        strToColor(command[4]));
+                            } else {
+                                field = new FieldClient(x, y, false, strToStatus(command[3]),
+                                        strToColor(command[4]));
+                            }
 
-						tc.setField(field);
-						break;
-					case "PLAYER":
-						int id = Integer.parseInt(command[1]);
-						x = Integer.parseInt(command[2]);
-						y = Integer.parseInt(command[3]);
-						boolean isDeath;
-						Direction dir = strToDirection(command[4]);
-						if (command[5].equals("DEAD")) {
-							isDeath = true;
-						} else {
-							isDeath = false;
-						}
+                            tc.setField(field);
+                            break;
+                        case "PLAYER":
+                            int id = Integer.parseInt(command[1]);
+                            x = Integer.parseInt(command[2]);
+                            y = Integer.parseInt(command[3]);
+                            boolean isDeath;
+                            Direction dir = strToDirection(command[4]);
+                            if (command[5].equals("DEAD")) {
+                                isDeath = true;
+                            } else {
+                                isDeath = false;
+                            }
 
-						tc.setHuman(new PlayerClient(id, x, y, dir, isDeath));
-						break;
-					case "READY":
-						tc.setColorPair(Integer.parseInt(command[2]));
-						tc.setPlayerId(Integer.parseInt(command[4]));
-						tc.setReady();
-						break;
-					case "FINISH": // 終了
-						tc.setFinish();
-						
-						break messageLoop; // メッセージ受信ループを抜ける
-					case "TIME": // 時間を送る
-						tc.setTime(Integer.parseInt(command[1]));
+                            tc.setHuman(new PlayerClient(id, x, y, dir, isDeath));
+                            break;
+                        case "READY":
+                            tc.setColorPair(Integer.parseInt(command[2]));
+                            tc.setPlayerId(Integer.parseInt(command[4]));
+                            tc.setReady();
+                            break;
+                        case "FINISH": // 終了
+                            tc.setFinish();
 
-						// TODO debug
-						// System.out.println("TIME:" + command[1]);
-						break;
-						
-						
-						// TODO tcからgetScoreするのは結合が強くなるのでまずそう
-					case "RESULT": // 結果を送る
-						// TODO debug
-						System.out.println("recieve result:" + inputLine);
+                            break messageLoop; // メッセージ受信ループを抜ける
+                        case "TIME": // 時間を送る
+                            tc.setTime(Integer.parseInt(command[1]));
 
-						switch (command[1]) {
-						case "PLAYERNUM":
-							tc.createScore(Integer.parseInt(command[2]));
-							break;
-						case "FIELD":
-							tc.getScore().painted[Integer.parseInt(command[2])] = Integer.parseInt(command[3]);
+                            // TODO debug
+                            // System.out.println("TIME:" + command[1]);
+                            break;
 
-							// TODO debug
-							System.out.println("recieve field:" + inputLine);
 
-							break;
-						case "KILL":
-							tc.getScore().kill[Integer.parseInt(command[2])] = Integer.parseInt(command[3]);
-							break;
-						case "DEATH":
-							tc.getScore().death[Integer.parseInt(command[2])] = Integer.parseInt(command[3]);
-							break;
-						}
-						break;
-					default:
-						System.err.println("MesgRecvThread: unknown command");
-						break;
-					}
+                        // TODO tcからgetScoreするのは結合が強くなるのでまずそう
+                        case "RESULT": // 結果を送る
+                            // TODO debug
+                            System.out.println("recieve result:" + inputLine);
 
-				} else {
-					break;
-				}
-			}
-			socket.close();
-		} catch (IOException e) {
-			System.err.println("エラーが発生しました: " + e);
-		}
-	}
+                            switch (command[1]) {
+                                case "PLAYERNUM":
+                                    tc.createScore(Integer.parseInt(command[2]));
+                                    break;
+                                case "FIELD":
+                                    tc.getScore().painted[Integer.parseInt(command[2])] =
+                                            Integer.parseInt(command[3]);
 
-	private Status strToStatus(String str) {
-		if (str.equals(Status.BOMBERING.name())) { // switch文はcase
-													// 定数:でなくてはならないのでif
-			return Status.BOMBERING;
-		} else if (str.equals(Status.BREAKABLE1.name())) {
-			return Status.BREAKABLE1;
-		} else if (str.equals(Status.BREAKABLE2.name())) {
-			return Status.BREAKABLE2;
-		} else if (str.equals(Status.BREAKABLE3.name())) {
-			return Status.BREAKABLE3;
-		} else if (str.equals(Status.MUTEKI.name())) {
-			return Status.MUTEKI;
-		} else if (str.equals(Status.NOTHING.name())) {
-			return Status.NOTHING;
-		} else {
-			return Status.UNBREAKABLE; // 本当は全てのそれ以外のelseで例外を投げたりしたいが・・・
-		}
-	}
+                                    // TODO debug
+                                    System.out.println("recieve field:" + inputLine);
 
-	private Color strToColor(String str) {
-		if (str.equals(Color.Blue.name())) {
-			return Color.Blue;
-		} else if (str.equals(Color.Green.name())) {
-			return Color.Green;
-		} else if (str.equals(Color.Red.name())) {
-			return Color.Red; // TODO 他の色の分も追加する
-		} else if (str.equals(Color.Transparent.name())) {
-			return Color.Transparent;
-		} else if (str.equals(Color.Purple.name())) {
-			return Color.Purple;
-		} else if (str.equals(Color.Orange.name())) {
-			return Color.Orange;
-		} else if (str.equals(Color.Pink.name())) {
-			return Color.Pink;
-		} else if (str.equals(Color.LimeGreen.name())) {
-			return Color.LimeGreen;
-		} else if (str.equals(Color.Turquoise.name())) {
-			return Color.Turquoise;
-		}
+                                    break;
+                                case "KILL":
+                                    tc.getScore().kill[Integer.parseInt(command[2])] =
+                                            Integer.parseInt(command[3]);
+                                    break;
+                                case "DEATH":
+                                    tc.getScore().death[Integer.parseInt(command[2])] =
+                                            Integer.parseInt(command[3]);
+                                    break;
+                            }
+                            break;
+                        default:
+                            System.err.println("MesgRecvThread: unknown command");
+                            break;
+                    }
 
-		else {
-			return null;
-		}
-	}
+                } else {
+                    break;
+                }
+            }
+            socket.close();
+        } catch (IOException e) {
+            System.err.println("エラーが発生しました: " + e);
+        }
+    }
 
-	private Direction strToDirection(String str) {
-		if (str.equals(Direction.LEFT.name())) { // switch文はcase
-													// 定数:でなくてはならないのでif
-			return Direction.LEFT;
-		} else if (str.equals(Direction.RIGHT.name())) {
-			return Direction.RIGHT;
-		} else if (str.equals(Direction.UP.name())) {
-			return Direction.UP;
-		} else if (str.equals(Direction.DOWN.name())) {
-			return Direction.DOWN;
-		} else if (str.equals(Direction.TURN_LEFT.name())) {
-			return Direction.TURN_LEFT;
-		} else if (str.equals(Direction.TURN_RIGHT.name())) {
-			return Direction.TURN_RIGHT;
-		} else if (str.equals(Direction.TURN_UP.name())) {
-			return Direction.TURN_UP;
-		} else if (str.equals(Direction.TURN_DOWN.name())) {
-			return Direction.TURN_DOWN;
-		} else {
-			return Direction.NONE;
-		}
+    private Status strToStatus(String str) {
+        if (str.equals(Status.BOMBERING.name())) { // switch文はcase
+                                                   // 定数:でなくてはならないのでif
+            return Status.BOMBERING;
+        } else if (str.equals(Status.BREAKABLE1.name())) {
+            return Status.BREAKABLE1;
+        } else if (str.equals(Status.BREAKABLE2.name())) {
+            return Status.BREAKABLE2;
+        } else if (str.equals(Status.BREAKABLE3.name())) {
+            return Status.BREAKABLE3;
+        } else if (str.equals(Status.MUTEKI.name())) {
+            return Status.MUTEKI;
+        } else if (str.equals(Status.NOTHING.name())) {
+            return Status.NOTHING;
+        } else {
+            return Status.UNBREAKABLE; // 本当は全てのそれ以外のelseで例外を投げたりしたいが・・・
+        }
+    }
 
-	}
+    private Color strToColor(String str) {
+        if (str.equals(Color.Blue.name())) {
+            return Color.Blue;
+        } else if (str.equals(Color.Green.name())) {
+            return Color.Green;
+        } else if (str.equals(Color.Red.name())) {
+            return Color.Red; // TODO 他の色の分も追加する
+        } else if (str.equals(Color.Transparent.name())) {
+            return Color.Transparent;
+        } else if (str.equals(Color.Purple.name())) {
+            return Color.Purple;
+        } else if (str.equals(Color.Orange.name())) {
+            return Color.Orange;
+        } else if (str.equals(Color.Pink.name())) {
+            return Color.Pink;
+        } else if (str.equals(Color.LimeGreen.name())) {
+            return Color.LimeGreen;
+        } else if (str.equals(Color.Turquoise.name())) {
+            return Color.Turquoise;
+        }
+
+        else {
+            return null;
+        }
+    }
+
+    private Direction strToDirection(String str) {
+        if (str.equals(Direction.LEFT.name())) { // switch文はcase
+                                                 // 定数:でなくてはならないのでif
+            return Direction.LEFT;
+        } else if (str.equals(Direction.RIGHT.name())) {
+            return Direction.RIGHT;
+        } else if (str.equals(Direction.UP.name())) {
+            return Direction.UP;
+        } else if (str.equals(Direction.DOWN.name())) {
+            return Direction.DOWN;
+        } else if (str.equals(Direction.TURN_LEFT.name())) {
+            return Direction.TURN_LEFT;
+        } else if (str.equals(Direction.TURN_RIGHT.name())) {
+            return Direction.TURN_RIGHT;
+        } else if (str.equals(Direction.TURN_UP.name())) {
+            return Direction.TURN_UP;
+        } else if (str.equals(Direction.TURN_DOWN.name())) {
+            return Direction.TURN_DOWN;
+        } else {
+            return Direction.NONE;
+        }
+    }
 }
--- src/client/network/TransmissionClient.java
+++ src/client/network/TransmissionClient.java
@@ -9,218 +9,217 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayDeque;
 import java.util.Queue;
-
 import client.constants.Direction;
 import client.data.FieldClient;
 import client.data.PlayerClient;
 import common.Setting;
 
 public class TransmissionClient {
-	public static String hostName = "localhost"; // TODO Config.update()から参照されている staticでやるのはあまり良くない
-	public static final int MAX_PLAYER = Setting.P; // プレイヤーの人数	
+    public static String hostName = "localhost"; // TODO Config.update()から参照されている staticでやるのはあまり良くない
+    public static final int MAX_PLAYER = Setting.P; // プレイヤーの人数
 
-	private PrintWriter out;// 出力用のライター
-	
-	private boolean ready = false; // サーバー側でゲームを開始する準備ができたか
-	private boolean isConnected = false; // サーバと接続が確立されたか
-	private boolean isFinish = false; // ゲームが終わったか
-	
-	private Queue<PlayerClient> recievedHumanBuffer;
-	private Queue<FieldClient> recievedFieldBuffer;
+    private PrintWriter out; // 出力用のライター
 
-	private int time; // 経過時間か何か
-	private int portnumber;
-	private Socket socket = null;
+    private boolean ready = false; // サーバー側でゲームを開始する準備ができたか
+    private boolean isConnected = false; // サーバと接続が確立されたか
+    private boolean isFinish = false; // ゲームが終わったか
 
-	private common.Score score; // 成績
-	private int colorPair; // 配色
-	private int playerId = -1; // プレイヤー番号
+    private Queue<PlayerClient> recievedHumanBuffer;
+    private Queue<FieldClient> recievedFieldBuffer;
+
+    private int time; // 経過時間か何か
+    private int portnumber;
+    private Socket socket = null;
+
+    private common.Score score; // 成績
+    private int colorPair; // 配色
+    private int playerId = -1; // プレイヤー番号
 
 
-	public TransmissionClient() {
-		this(hostName, server.network.TransmissionServer.PORT);
-	}
+    public TransmissionClient() {
+        this(hostName, server.network.TransmissionServer.PORT);
+    }
 
-	public TransmissionClient(String hostname, int portnumber) {
-		recievedHumanBuffer = new ArrayDeque<PlayerClient>();
-		recievedFieldBuffer = new ArrayDeque<FieldClient>();
+    public TransmissionClient(String hostname, int portnumber) {
+        recievedHumanBuffer = new ArrayDeque<PlayerClient>();
+        recievedFieldBuffer = new ArrayDeque<FieldClient>();
 
-		TransmissionClient.hostName = hostname;
-		this.portnumber = portnumber;
-	}
+        TransmissionClient.hostName = hostname;
+        this.portnumber = portnumber;
+    }
 
-	// サーバーに接続
-	/*
-	 * TODO 例外処理が適当すぎる．例外もきちんと対処出来るようにする
-	 */
-	public boolean openConection() throws ConnectException {		
-		if (isConnected)
-			return true;		
-		try {
-			socket = new Socket(TransmissionClient.hostName, this.portnumber);
-		} catch (UnknownHostException e) {
-			System.err.println("ホストの IP アドレスが判定できません: " + e);
-		} catch (ConnectException e) { // サーバーが立っていない
-			throw e;
-		} catch (IOException e) {
-			System.err.println("エラーが発生しました: " + e);
-		}
-		
-		try {
-			out = new PrintWriter(socket.getOutputStream(), true);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		String responce = null; // サーバーからの応答
+    // サーバーに接続
+    /*
+     * TODO 例外処理が適当すぎる．例外もきちんと対処出来るようにする
+     */
+    public boolean openConection() throws ConnectException {
+        if (isConnected)
+            return true;
+        try {
+            socket = new Socket(TransmissionClient.hostName, this.portnumber);
+        } catch (UnknownHostException e) {
+            System.err.println("ホストの IP アドレスが判定できません: " + e);
+        } catch (ConnectException e) { // サーバーが立っていない
+            throw e;
+        } catch (IOException e) {
+            System.err.println("エラーが発生しました: " + e);
+        }
 
-		try {	
-			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			while ((responce = br.readLine()) == null) 
-				System.out.println("waiting for responce"); // TODO debug
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-		System.out.println(responce); // TODO debug
-				
-		if (responce.equals("BUSY")) { // 戦闘中だったら
-			try {
-				socket.close(); // TODO 毎回作って閉じるのは無駄だな・・・
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return false;
-		} else if (responce.equals("CONNECTED")) { // 戦闘中ではなかったら
-			// メッセージ受信用のスレッド作成
-			MesgRecvThread mrt = new MesgRecvThread(this, socket);
-			mrt.start();
-			isConnected = true;
-			return true;
-		} else {
-			System.err.println("unknown responce message");
-			System.exit(-1);
-			return true; // この行は実行されない
-		}
-	}
-	
-	// サーバとの接続を切断
-	public void closeConnection() {
-		try {
-			socket.close();
-			isConnected = false;
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	// send
-	public void sendHuman(Direction dir) {
-		out.println(dir.name());
-	}
+        try {
+            out = new PrintWriter(socket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String responce = null; // サーバーからの応答
 
-	public void sendBomb() {
-		out.println("BOMB");
-	}
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            while ((responce = br.readLine()) == null)
+                System.out.println("waiting for responce"); // TODO debug
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-	// receive
-	public FieldClient recieveField() {
-		if (recievedFieldBuffer.isEmpty()) {
-			return null;
-		} else {
-			return recievedFieldBuffer.remove();
-		}
-	}
+        System.out.println(responce); // TODO debug
 
-	public PlayerClient recievedHuman() {
-		if (recievedHumanBuffer.isEmpty()) {
-			return null;
-		} else {
-			return recievedHumanBuffer.remove();
-		}
-	}
+        if (responce.equals("BUSY")) { // 戦闘中だったら
+            try {
+                socket.close(); // TODO 毎回作って閉じるのは無駄だな・・・
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return false;
+        } else if (responce.equals("CONNECTED")) { // 戦闘中ではなかったら
+            // メッセージ受信用のスレッド作成
+            MesgRecvThread mrt = new MesgRecvThread(this, socket);
+            mrt.start();
+            isConnected = true;
+            return true;
+        } else {
+            System.err.println("unknown responce message");
+            System.exit(-1);
+            return true; // この行は実行されない
+        }
+    }
 
-	// （残り，経過）時間を取得
-	public int receiveTime() {
-		return time;
-	}
+    // サーバとの接続を切断
+    public void closeConnection() {
+        try {
+            socket.close();
+            isConnected = false;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-	// 終了かを取得
-	public boolean recieveFinish() {
-		return isFinish;
-	}
-	
-	public int recieveColorPair() {
-		return this.colorPair;
-	}
-	
-	public int recievePlayerId() {
-		return playerId;
-	}
-	
-	public int recieveTeamNumber() {
-		return playerId % 2;
-	}
-	
-	public boolean isReady() {
-		return ready;
-	}
+    // send
+    public void sendHuman(Direction dir) {
+        out.println(dir.name());
+    }
 
-	
-	/*
-	 * TODO アクセス修飾子をつける
-	 */
-	// MesgRecvThreadから呼ぶ
-	public void setHostName(String hostName){
-		this.hostName=hostName;
-	}
+    public void sendBomb() {
+        out.println("BOMB");
+    }
 
-	public void setField(FieldClient field) {
-		recievedFieldBuffer.offer(field);
-	}
+    // receive
+    public FieldClient recieveField() {
+        if (recievedFieldBuffer.isEmpty()) {
+            return null;
+        } else {
+            return recievedFieldBuffer.remove();
+        }
+    }
 
-	public void setHuman(PlayerClient human) {
-		recievedHumanBuffer.offer(human);
-	}
+    public PlayerClient recievedHuman() {
+        if (recievedHumanBuffer.isEmpty()) {
+            return null;
+        } else {
+            return recievedHumanBuffer.remove();
+        }
+    }
 
-	public void setReady() {
-		ready = true;
-	}
+    // （残り，経過）時間を取得
+    public int receiveTime() {
+        return time;
+    }
 
-	public void setFinish() {
-		isFinish = true;
-		closeConnection(); // ここで接続を切る
-	}
+    // 終了かを取得
+    public boolean recieveFinish() {
+        return isFinish;
+    }
 
-	public void setTime(int time) {
-		this.time = time;
-	}
-	
-	public void setPlayerId(int playerId) {
-		this.playerId = playerId;
-	}
-	
-	// 成績を受け取る scoreにまだ何も入ってなかったらnull返すよ！
-	// TODO nullはなるべく返さないようにしたい．nullオブジェクトとか使うか
-	public common.Score recieveScore() {
-		if (score == null) {
-			System.err.println("TransmissionClient: warning: score is null");
-		}
-		return score;
-	}
+    public int recieveColorPair() {
+        return this.colorPair;
+    }
 
-	public void createScore(int playerNum) {
-		score = new common.Score(playerNum);
-	}
+    public int recievePlayerId() {
+        return playerId;
+    }
 
-	public common.Score getScore() {
-		if (this.score == null) {
-			System.err.println("tc: warning, score is null");
-		}
+    public int recieveTeamNumber() {
+        return playerId % 2;
+    }
 
-		return score;
-	}
+    public boolean isReady() {
+        return ready;
+    }
 
-	public void setColorPair(int colorPair) {
-		this.colorPair = colorPair;
-	}
+
+    /*
+     * TODO アクセス修飾子をつける
+     */
+    // MesgRecvThreadから呼ぶ
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public void setField(FieldClient field) {
+        recievedFieldBuffer.offer(field);
+    }
+
+    public void setHuman(PlayerClient human) {
+        recievedHumanBuffer.offer(human);
+    }
+
+    public void setReady() {
+        ready = true;
+    }
+
+    public void setFinish() {
+        isFinish = true;
+        closeConnection(); // ここで接続を切る
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    // 成績を受け取る scoreにまだ何も入ってなかったらnull返すよ！
+    // TODO nullはなるべく返さないようにしたい．nullオブジェクトとか使うか
+    public common.Score recieveScore() {
+        if (score == null) {
+            System.err.println("TransmissionClient: warning: score is null");
+        }
+        return score;
+    }
+
+    public void createScore(int playerNum) {
+        score = new common.Score(playerNum);
+    }
+
+    public common.Score getScore() {
+        if (this.score == null) {
+            System.err.println("tc: warning, score is null");
+        }
+
+        return score;
+    }
+
+    public void setColorPair(int colorPair) {
+        this.colorPair = colorPair;
+    }
 }
