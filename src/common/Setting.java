3c60639e59cc198f90d900644a5d3c10f0805cf8
mofumofuchan <you_oki1@yahoo.co.jp>
2021-01-25 22:37:22 +0900

    Google Java Style Guideに従ってスタイルを修正

--- src/common/Setting.java
+++ src/common/Setting.java
@@ -3,14 +3,14 @@ package common;
 import server.constants.Color;
 
 public class Setting {
-	public static final int P = 4;
-	public static int[] STAGE4ONE = FieldData.fieldDate1;
-	public static int[] STAGE4TWO = FieldData.fieldDate1;
-	public static Color ColorTeam1 = Color.Blue;
-	public static Color ColorTeam2 = Color.Blue;
+    public static final int P = 4;
+    public static int[] STAGE4ONE = FieldData.fieldDate1;
+    public static int[] STAGE4TWO = FieldData.fieldDate1;
+    public static Color ColorTeam1 = Color.Blue;
+    public static Color ColorTeam2 = Color.Blue;
 
-	public static void setTeamColor(Color[] cl) {
-		ColorTeam1 = cl[0];
-		ColorTeam2 = cl[1];
-	}
+    public static void setTeamColor(Color[] cl) {
+        ColorTeam1 = cl[0];
+        ColorTeam2 = cl[1];
+    }
 }
