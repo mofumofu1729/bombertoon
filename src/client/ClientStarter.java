2fd964a45b53c9c136b4337da5f747b143dca120
mofumofuchan <you_oki1@yahoo.co.jp>
2021-01-25 21:38:17 +0900

    Google Java Style Guideに従ってフォーマットを修正

--- src/client/ClientStarter.java
+++ src/client/ClientStarter.java
@@ -19,70 +19,72 @@ import client.scenes.SceneTop;
  * クライアントアプリケーションのエントリーポイントを含むクラスです．
  */
 public class ClientStarter extends StateBasedGame {
-	public final static int FPS = 60;
-	private static TransmissionClient tc; // 一つのクライアントで一つの送信機
+    public static final int FPS = 60;
+    private static TransmissionClient tc; // 一つのクライアントで一つの送信機
 
-	public ClientStarter(String name) throws IOException {
-		super(name);
-		this.addState(new SceneGame(State.GAMECLIENT));
-		this.addState(new SceneTop(State.TOP));
-		this.addState(new SceneResult(State.RESULT));
+    public ClientStarter(String name) throws IOException {
+        super(name);
+        this.addState(new SceneGame(State.GAMECLIENT));
+        this.addState(new SceneTop(State.TOP));
+        this.addState(new SceneResult(State.RESULT));
 
-		this.addState(new SceneMenu(State.MENU));
-		this.addState(new SceneConfig(State.CONFIG));
-		this.addState(new SceneCredit(State.CREDIT));
-		this.addState(new SceneMatching(State.MATCHING));
-	}
+        this.addState(new SceneMenu(State.MENU));
+        this.addState(new SceneConfig(State.CONFIG));
+        this.addState(new SceneCredit(State.CREDIT));
+        this.addState(new SceneMatching(State.MATCHING));
+    }
 
-	public void initStatesList(GameContainer gc) throws SlickException {
-		this.getState(State.GAMECLIENT).init(gc, this);
-		this.getState(State.TOP).init(gc, this);
-		this.getState(State.RESULT).init(gc, this);
+    public void initStatesList(GameContainer gc) throws SlickException {
+        this.getState(State.GAMECLIENT).init(gc, this);
+        this.getState(State.TOP).init(gc, this);
+        this.getState(State.RESULT).init(gc, this);
 
-		this.getState(State.MENU).init(gc, this);
-		this.getState(State.CONFIG).init(gc, this);
-		this.getState(State.CREDIT).init(gc, this);
-		this.getState(State.MATCHING).init(gc, this);
-		this.getState(State.RATING).init(gc, this);
-		this.getState(State.CONFIG).init(gc, this);
+        this.getState(State.MENU).init(gc, this);
+        this.getState(State.CONFIG).init(gc, this);
+        this.getState(State.CREDIT).init(gc, this);
+        this.getState(State.MATCHING).init(gc, this);
+        this.getState(State.RATING).init(gc, this);
+        this.getState(State.CONFIG).init(gc, this);
 
-		// this.enterState(State.GAMECLIENT);
+        // this.enterState(State.GAMECLIENT);
 
-		this.enterState(State.TOP);
+        this.enterState(State.TOP);
 
-	}
+    }
 
-	public static void main(String[] args) throws SlickException, IOException {
-		// TODO ちょっとこの行は怪しいので後で直す
-		/* System.setProperty("org.lwjgl.librarypath",
-				new File(new File(System.getProperty("user.dir"), "native"), LWJGLUtil.getPlatformName())
-						.getAbsolutePath()); */
+    public static void main(String[] args) throws SlickException, IOException {
+        // TODO ちょっとこの行は怪しいので後で直す
+        /*
+         * System.setProperty("org.lwjgl.librarypath", new File(new
+         * File(System.getProperty("user.dir"), "native"), LWJGLUtil.getPlatformName())
+         * .getAbsolutePath());
+         */
 
-		AppGameContainer app = new AppGameContainer(new ClientStarter("client"));
-		String[] icons = {"res/icons/icon16.png","res/icons/icon32.png","res/icons/icon48.png"};
+        AppGameContainer app = new AppGameContainer(new ClientStarter("client"));
+        String[] icons = {"res/icons/icon16.png", "res/icons/icon32.png", "res/icons/icon48.png"};
 
-		app.setIcons(icons);
-		app.setDisplayMode(800, 600, false);
-		app.setTargetFrameRate(FPS);
-		app.start();
-	}
+        app.setIcons(icons);
+        app.setDisplayMode(800, 600, false);
+        app.setTargetFrameRate(FPS);
+        app.start();
+    }
 
-	// TransmissionClinet
+    // TransmissionClinet
 
-	public static void createTransmissionClient() {
-		ClientStarter.tc = new TransmissionClient();
-	}
+    public static void createTransmissionClient() {
+        ClientStarter.tc = new TransmissionClient();
+    }
 
-	public static void createTransmissionClient(String hostname, int portNumber) {
-		ClientStarter.tc = new TransmissionClient(hostname, portNumber);
-	}
+    public static void createTransmissionClient(String hostname, int portNumber) {
+        ClientStarter.tc = new TransmissionClient(hostname, portNumber);
+    }
 
-	public static TransmissionClient getTransmissionClient() {
-		if (tc == null) {
-			System.err.println("client-server tc null");
-		}
+    public static TransmissionClient getTransmissionClient() {
+        if (tc == null) {
+            System.err.println("client-server tc null");
+        }
 
-		return ClientStarter.tc;
-	}
+        return ClientStarter.tc;
+    }
 
 }
