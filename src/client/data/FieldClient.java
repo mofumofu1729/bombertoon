38d7233c4b2d948dd96b18d9bc31d70d23765fc4
mofumofuchan <you_oki1@yahoo.co.jp>
2021-01-25 21:44:50 +0900

    Google Java Style Guideに従ってスタイルを修正

--- src/client/data/FieldClient.java
+++ src/client/data/FieldClient.java
@@ -4,19 +4,19 @@ import client.constants.Status;
 import server.constants.Color;
 
 public class FieldClient {
-	public int x;
-	public int y;
-	public boolean isExistBomb;
-	public int bombCount = 2800;
-	public Status status;
-	public Color color;
+    public int x;
+    public int y;
+    public boolean isExistBomb;
+    public int bombCount = 2800;
+    public Status status;
+    public Color color;
 
 
-	public FieldClient(int x, int y, boolean isExistBomb, Status status, Color color) {
-		this.x = x;
-		this.y = y;
-		this.isExistBomb = isExistBomb;
-		this.status = status;
-		this.color = color;
-	}
+    public FieldClient(int x, int y, boolean isExistBomb, Status status, Color color) {
+        this.x = x;
+        this.y = y;
+        this.isExistBomb = isExistBomb;
+        this.status = status;
+        this.color = color;
+    }
 }
--- src/client/data/PlayerClient.java
+++ src/client/data/PlayerClient.java
@@ -3,21 +3,23 @@ package client.data;
 import client.constants.Direction;
 
 public class PlayerClient {
-	public int playerID;
-	public int x;
-	public int y;
-	public Direction dir; // 向き
-	public boolean isDeath;
+    public int playerID;
+    public int x;
+    public int y;
+    public Direction dir; // 向き
+    public boolean isDeath;
 
-	public PlayerClient(int playerID, int x, int y, Direction dir, boolean isDeath) {
-		this.playerID = playerID;
-		this.x = x;
-		this.y = y;
-		this.dir = dir;
-		this.isDeath = isDeath;
-	}
-	
-	public String toString() {
-		return ("playerID:"+playerID+",x:"+x+",y:"+",dir:"+dir.name()+",isDeath:"+isDeath);
-	}
+
+    public PlayerClient(int playerID, int x, int y, Direction dir, boolean isDeath) {
+        this.playerID = playerID;
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+        this.isDeath = isDeath;
+    }
+
+    public String toString() {
+        return ("playerID:" + playerID + ",x:" + x + ",y:" + ",dir:" + dir.name() + ",isDeath:"
+                + isDeath);
+    }
 }
