f965430a38053bdb5c43dd3d2e9f4205f7e4783f
mofumofuchan <you_oki1@yahoo.co.jp>
2021-01-25 21:43:29 +0900

    Google Java Style Guideに従ってスタイルを修正

--- src/client/constants/Direction.java
+++ src/client/constants/Direction.java
@@ -1,6 +1,5 @@
 package client.constants;
 
-
 public enum Direction {
-	UP, DOWN, RIGHT, LEFT, TURN_UP, TURN_DOWN, TURN_RIGHT, TURN_LEFT, NONE;
+    UP, DOWN, RIGHT, LEFT, TURN_UP, TURN_DOWN, TURN_RIGHT, TURN_LEFT, NONE;
 }
--- src/client/constants/State.java
+++ src/client/constants/State.java
@@ -1,18 +1,14 @@
 package client.constants;
 
 public class State {
-	public static final int GAMECLIENT = 0; // 名前変更しました！！！
-	public static final int TOP = 1;
-	public static final int RESULT = 2;
-
-	// 追加
-	public static final int MENU = 3;
-
-	public static final int RATING = 5;
-	public static final int MATCHING = 6;
-	public static final int CREDIT = 7; // スタッフロール
-	public static final int ONEON1 = 8;
-	public static final int TWOON2 = 9;
-	public static final int CONFIG = 10;
-
+    public static final int GAMECLIENT = 0; // 名前変更しました！！！
+    public static final int TOP = 1;
+    public static final int RESULT = 2;
+    public static final int MENU = 3;
+    public static final int RATING = 5;
+    public static final int MATCHING = 6;
+    public static final int CREDIT = 7; // スタッフロール
+    public static final int ONEON1 = 8;
+    public static final int TWOON2 = 9;
+    public static final int CONFIG = 10;
 }
