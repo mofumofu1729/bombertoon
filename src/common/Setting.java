226aa90651dcccb70a19127f60ecf00e91a0404b
mofumofuchan <you_oki1@yahoo.co.jp>
2021-01-29 00:13:42 +0900

    チーム数を定数に変更

--- src/common/Setting.java
+++ src/common/Setting.java
@@ -4,6 +4,7 @@ import server.constants.Color;
 
 public class Setting {
     public static final int N_PLAYERS = 1;  // FIXME 一時的に一人だけに変更
+    public static final int N_TEAMS = 2;
     public static int[] STAGE4ONE = FieldData.fieldDate1;
     public static int[] STAGE4TWO = FieldData.fieldDate1;
     public static Color ColorTeam1 = Color.Blue;
