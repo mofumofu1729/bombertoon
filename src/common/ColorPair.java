e031473e9b2136bf4267e67e0371b02e67e4c414
mofumofuchan <you_oki1@yahoo.co.jp>
2021-01-29 00:34:08 +0900

    変数名などのリファクタリング

--- src/common/ColorPair.java
+++ src/common/ColorPair.java
@@ -2,30 +2,43 @@ package common;
 
 import server.constants.Color;
 
+/**
+ * チームの色の組を持つクラス.
+ */
 public class ColorPair {
     public static final int N_COLOR_PAIRS = 4;  // 使用可能な色の組の数
 
-    static public Color[] getColorPair(int getNumber) {
-        Color[] cl = new Color[2];
-        switch (getNumber) {
+    /**
+     * チームの色の組を取得.
+     *
+     * @param teamNumber チーム番号
+     * @return チームの色の組
+     */
+    public static Color[] getColorPair(int teamNumber) {
+        Color[] teamColorPair = new Color[2];
+
+        switch (teamNumber) {
             case 0:
-                cl[0] = Color.Blue;
-                cl[1] = Color.Orange;
+                teamColorPair[0] = Color.Blue;
+                teamColorPair[1] = Color.Orange;
                 break;
             case 1:
-                cl[0] = Color.Pink;
-                cl[1] = Color.Turquoise;
+                teamColorPair[0] = Color.Pink;
+                teamColorPair[1] = Color.Turquoise;
                 break;
             case 2:
-                cl[0] = Color.Purple;
-                cl[1] = Color.LimeGreen;
+                teamColorPair[0] = Color.Purple;
+                teamColorPair[1] = Color.LimeGreen;
                 break;
             case 3:
-                cl[0] = Color.Red;
-                cl[1] = Color.Green;
+                teamColorPair[0] = Color.Red;
+                teamColorPair[1] = Color.Green;
                 break;
+            default:
+                System.err.println("invalid team number: " + teamNumber);
+                System.exit(1);
         }
 
-        return cl;
+        return teamColorPair;
     }
 }
